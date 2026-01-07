e555760300836118f90e656c442660ecfa0c013a
Yukiel <140352744+YukielCr@users.noreply.github.com>
2026-01-06 20:02:46 -0600

    Donde me quede en el video

--- cli.js
+++ cli.js
@@ -4,4 +4,5 @@ console.log("Hello World!".bgGreen)
 console.log("yuki.com".bgBlue)
 console.log("google.com".bgYellow)
 
-npm i lodash dayjs bootstrap
+// minuto 2:23:05
+// Node curso practico desde 0
