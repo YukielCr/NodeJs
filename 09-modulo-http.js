6f82afb9d7144f82cb1ca35399d1cbb4eb0950d5
Yukiel <kevinyukielcoronaramos@gmail.com>
2026-02-03 13:44:29 -0600

    Guardando cambios

--- 09-modulo-http.js
+++ 09-modulo-http.js
@@ -64,6 +64,7 @@ const server = http.createServer((request, response) => {
     response.write(`
         <h1>404 Not Found</h1>
         <p>The page you are looking for does not exist</p>
+        <p>This is a missteck</p>
         <nav>
             <a href="/">Inicio</a>
             <br>
